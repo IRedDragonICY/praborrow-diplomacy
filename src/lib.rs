98106a5f457fae54fe0e097c740c2c8fda2fa782
Lead Architect <architect@praborrow.internal>
2026-01-09 22:12:18 +0700

    chore: Upgrade to Rust 2024 (fix no_mangle) and bump v0.1.1

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 // Praborrow Diplomacy
 // FFI Stubs for international relations (Foreign Functions).
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn establish_relations() {
     println!("Diplomatic relations established.");
 }
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 pub extern "C" fn send_envoy(id: u32) {
     println!("Envoy {} sent to foreign jurisdiction.", id);
 }
